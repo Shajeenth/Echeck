627fb25728af6284a6fc23ba7fa998995440b01f
Shajeenth <shajeenths@gmail.com>
2025-07-05 00:50:08 -0400

    Added animation to PlayOptions.tsx

--- src/pages/components/PlayOptions.tsx
+++ src/pages/components/PlayOptions.tsx
@@ -2,11 +2,11 @@ import "./index.css";
 
 function PlayOptions({ emoji, title, desc }: { emoji: string; title: string; desc: string }) {
     return (
-        <div className="bg-gray-100 m-4 p-8 rounded-lg shadow-lg">
+        <div className="bg-gray-100 m-4 p-8 rounded-lg shadow-lg duration-300 hover:-translate-y-4">
             <div className="">
-                {emoji}
+                <h1 className="text-5xl mb-4">{emoji}</h1>
                 <h1 className="text-3xl mb-4 boldonse-regular">{title}</h1>
-                <p className="text-lg text-gray-700 mb-8">{desc}</p>
+                <p className="text-lg text-gray-700">{desc}</p>
             </div> 
         </div>
     )
